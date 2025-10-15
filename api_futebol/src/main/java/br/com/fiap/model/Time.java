2d10bf2a7f63b3c5da7f33f4563046d958312eb1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-15 09:08:43 -0300

    API Campeonato de Futebol

--- api_futebol/src/main/java/br/com/fiap/model/Time.java
+++ api_futebol/src/main/java/br/com/fiap/model/Time.java
@@ -7,5 +7,42 @@ public class Time {
     private String tecnico;
     private int anoFundacao;
 
-    
+    public Time(String nome, String estadio, String tecnico, int anoFundacao) {
+        this.nome = nome;
+        this.estadio = estadio;
+        this.tecnico = tecnico;
+        this.anoFundacao = anoFundacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public String getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public int getAnoFundacao() {
+        return anoFundacao;
+    }
+
+    public void setAnoFundacao(int anoFundacao) {
+        this.anoFundacao = anoFundacao;
+    }
 }
--- api_futebol/src/main/java/br/com/fiap/resource/CampeonatoResource.java
+++ api_futebol/src/main/java/br/com/fiap/resource/CampeonatoResource.java
@@ -1,19 +1,21 @@
 package br.com.fiap.resource;
 
 import br.com.fiap.model.Time;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/campeonato")
 public class CampeonatoResource {
 
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getTime() {
         Time time = new Time("Palmeiras", "Allianz Park", "Abel Ferreira", 1901);
-
-
-
-        return Response.ok().build();
+        return Response.ok(time).build();
     }
 
 
