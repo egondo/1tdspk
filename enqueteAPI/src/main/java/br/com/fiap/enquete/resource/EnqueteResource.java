53d6a0bb4110b981cd04b3f2260ff5ca97f61e28
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-22 09:52:50 -0300

    Outras opcoes de API: lista de objetos e PathParam

--- enqueteAPI/src/main/java/br/com/fiap/enquete/resource/EnqueteResource.java
+++ enqueteAPI/src/main/java/br/com/fiap/enquete/resource/EnqueteResource.java
@@ -2,12 +2,14 @@ package br.com.fiap.enquete.resource;
 
 import br.com.fiap.enquete.business.EnqueteBusiness;
 import br.com.fiap.enquete.model.Enquete;
+import br.com.fiap.enquete.model.Item;
 import br.com.fiap.enquete.model.Pergunta;
 import br.com.fiap.enquete.util.PerguntaTO;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/enquetes")
@@ -15,12 +17,12 @@ public class EnqueteResource {
 
 
     @GET
-    @Path("/perguntas")
+    @Path("/perguntas/{idEnquete}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getPerguntas() {
+    public Response getPerguntas(@PathParam("idEnquete") long idEnquete) {
         EnqueteBusiness neg = new EnqueteBusiness();
         Enquete enq = new Enquete();
-        enq.setId(8);
+        enq.setId(idEnquete);
         List<PerguntaTO> lista = null;
         try {
             lista = neg.recuperaPerguntas(enq);
@@ -33,14 +35,14 @@ public class EnqueteResource {
     }
 
     @POST
-    @Path("/perguntas")
+    @Path("/perguntas/{idEnquete}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response cadastraPergunta(PerguntaTO p) {
+    public Response cadastraPergunta(PerguntaTO p, @PathParam("idEnquete")long idEnquete) {
         //System.out.println(p.getNumero() + " " + p.getEnunciado());
         //System.out.println(p.getDescricaoItem());
         //System.out.println(p.getTipo());
         Enquete enq = new Enquete();
-        enq.setId(8);
+        enq.setId(idEnquete);
 
         Pergunta pergunta = new Pergunta();
         pergunta.setTipo(p.getTipo());
@@ -59,6 +61,44 @@ public class EnqueteResource {
         }
         return Response.status(201).entity(pergunta).build();
     }
+
+    @POST
+    @Path("/perguntas/multiplas/{idEnquete}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response cadastraPerguntaMultipla(List<PerguntaTO> lista, @PathParam("idEnquete")long idEnquete) {
+        Enquete enq = new Enquete();
+        enq.setId(idEnquete);
+
+        Pergunta pergunta = new Pergunta();
+        List<Item> itens = new ArrayList<>();
+
+        for(PerguntaTO p : lista) {
+            pergunta.setTipo(p.getTipo());
+            pergunta.setNumero(p.getNumero());
+            pergunta.setEnunciado(p.getEnunciado());
+            pergunta.setEnquete(enq);
+            Item item = new Item();
+            item.setDescricao(p.getDescricaoItem());
+            itens.add(item);
+        }
+
+        pergunta.setItens(itens);
+        EnqueteBusiness negocio = new EnqueteBusiness();
+
+        try {
+            negocio.cadastraPergunta(pergunta);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(401).entity(e.getMessage()).build();
+            //throw new RuntimeException(e);
+        }
+        return Response.status(201).entity(pergunta).build();
+    }
+
+
+
+
+
 
     @PUT
     @Path("/perguntas")
